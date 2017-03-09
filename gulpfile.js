2c99ecb242c51e29f3880910fed7646efdc347d8
Мурашко Александр <amurashko@conteq.ru>
2017-03-09 18:15:08 +0300

    initial

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+/* gulpfile.js */
+var
+    gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat');
+
+// source and distribution folder
+var
+    source = 'src/',
+    dest = 'dist/';
+
+// Bootstrap scss source
+var bootstrapSass = { in: './bower_components/bootstrap-sass/'
+};
+
+// fonts
+var fonts = { in: [source + 'fonts/*.*', bootstrapSass.in + 'assets/fonts/**/*'],
+    out: dest + 'fonts/'
+};
+
+// css source file: .scss files
+var css = { in: source + 'css/main.scss',
+    out: dest + 'css/',
+    watch: source + 'css/**/*',
+    add: source + 'css/bootstrap-custom.css',
+    sassOpts: {
+        outputStyle: 'nested',
+        precison: 3,
+        errLogToConsole: true,
+        includePaths: [bootstrapSass.in + 'assets/stylesheets']
+    }
+};
+
+// copy bootstrap required fonts to dest
+gulp.task('fonts', function() {
+    return gulp
+        .src(fonts.in)
+        .pipe(gulp.dest(fonts.out));
+});
+
+// copy additional css to dest
+gulp.task('additional', function() {
+    return gulp
+        .src(css.add)
+        .pipe(gulp.dest(css.out));
+});
+
+// compile scss
+gulp.task('sass', ['fonts'], function() {
+    return gulp.src(css.in)
+        .pipe(sass(css.sassOpts))
+        .pipe(gulp.dest(css.out));
+});
+
+// default task
+gulp.task('default', ['sass', 'additional'], function() {
+    gulp.watch(css.watch, ['sass']);
+});
