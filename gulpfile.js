206c4978ced0f622e0d8cac1f4e8096ee83848a1
modano8 <dustyhell@gmail.com>
2017-03-10 16:50:46 +0300

    gulp-connect

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@
 var
     gulp = require('gulp'),
     sass = require('gulp-sass'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    livereload = require('gulp-livereload'),
+    connect = require('gulp-connect');
 
 // source and distribution folder
 var
@@ -18,6 +20,12 @@ var fonts = { in: [source + 'fonts/*.*', bootstrapSass.in + 'assets/fonts/**/*']
     out: dest + 'fonts/'
 };
 
+// html
+var html = { in: source + '*.html',
+    out: dest,
+    watch: source + '*.html'
+};
+
 // css source file: .scss files
 var css = { in: source + 'css/main.scss',
     out: dest + 'css/',
@@ -30,6 +38,14 @@ var css = { in: source + 'css/main.scss',
         includePaths: [bootstrapSass.in + 'assets/stylesheets']
     }
 };
+
+// copy html to dest
+gulp.task('html', function() {
+    return gulp
+        .src(html.in)
+        .pipe(gulp.dest(html.out))
+        .pipe(connect.reload());
+});
 
 // copy bootstrap required fonts to dest
 gulp.task('fonts', function() {
@@ -53,7 +69,17 @@ gulp.task('sass', ['fonts'], function() {
         .pipe(gulp.dest(css.out));
 });
 
+// server connect
+gulp.task('connect', function() {
+    connect.server({
+        root: [__dirname],
+        livereload: true
+    });
+});
+
 // default task
-gulp.task('default', ['sass'], function() {
+gulp.task('default', ['connect', 'sass', 'html'], function() {
+
     gulp.watch(css.watch, ['sass']);
+    gulp.watch(html.watch, ['html']);
 });
